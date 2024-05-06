50a2b7e5e41ce8f8c4a2528399566ffcd6faaf0e
kaiomaced0 <kaiomm2000@gmail.com>
2024-05-06 20:47:26 -0300

    add proxy - usar futuramente

---
+++ proxy.conf.js
@@ -0,0 +1,10 @@
+const PROXY_CONF = [
+    // {
+    //   context: ['/auth'],
+    //   target: 'http://localhost:8082/',
+    //   secure: false,
+    //   logLevel: 'debug'
+    // },
+  ];
+
+  module.exports = PROXY_CONF;
