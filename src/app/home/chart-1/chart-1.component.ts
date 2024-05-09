ac076a01c73556da0b9fd84d77e4d968f54edd48
05562849259 <kaiomm2000@gmail.com>
2024-05-09 18:07:33 -0300

    add tipo de chart ng

--- src/app/home/chart-1/chart-1.component.ts
+++ src/app/home/chart-1/chart-1.component.ts
@@ -9,36 +9,42 @@ import { ChartModule } from 'primeng/chart';
   styleUrls: ['./chart-1.component.css']
 })
 export class Chart1Component implements OnInit {
-
-  @Input() data1!: { labels: string[], values: number[] };
-
+  @Input() data1!: { [key: string]: number };
+  @Input() descricao?: string;
+  @Input() tipo?: string;
   data: any;
   options: any;
 
-  ngOnInit() {
-      const documentStyle = getComputedStyle(document.documentElement);
-      const textColor = documentStyle.getPropertyValue('--text-color');
+ngOnInit() {
+  // Remover a chave "Total" e extrair os labels e values
+  const { Total, ...chartData } = this.data1;
+  const labels = Object.keys(chartData);
+  const values = Object.values(chartData);
 
-      this.data = {
-          labels: [...this.data1.labels],
-          datasets: [
-              {
-                  label: 'Dataset 1',
-                  data: this.data1.values,
-                  backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-              }
-          ]
-      };
 
-      this.options = {
-          cutout: '60%',
-          plugins: {
-              legend: {
-                  labels: {
-                      color: textColor
-                  }
-              }
-          }
-      };
-  }
+  const documentStyle = getComputedStyle(document.documentElement);
+  const textColor = documentStyle.getPropertyValue('--text-color');
+
+  this.data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Procedimentos Realizados',
+        data: values,
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#86796D', '#9ABACF', '#66323D', '#954BC0', '#FFA256'],
+      }
+    ]
+  };
+
+  this.options = {
+    cutout: '60%',
+    plugins: {
+      legend: {
+        labels: {
+          color: textColor
+        }
+      }
+    }
+  };
+}
 }
